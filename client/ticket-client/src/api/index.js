768d4ab4c3c149469d4f1171532101e2a2119074
Luke Tarr <lrt22@pitt.edu>
2022-01-06 17:32:13 -0500

    front end changes

--- client/ticket-client/src/api/index.js
+++ client/ticket-client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL : "http://tckt-demo.herokuapp.com"});
+const API = axios.create({baseURL : "http://localhost:4000"});
 
 export const signin = (formData) => API.post("/auth/login", formData);
 export const signup = (formData) => API.post("/auth/register", formData);
